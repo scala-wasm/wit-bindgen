6754cadca5a8fa6c776cec9a54aee93bf044913d
Rikito Taniguchi <rikiriki1238@gmail.com>
2026-01-09 00:14:51 +0900

    Replace case class/object with regular class/object
    Replace case class and case object with regular class and object in
    generated Scala WIT bindings to make javalib independent from the
    Scala standard library. (oops, except unapply method signature)
    
    This allows the generated bindings to be used in scala-wasm's javalib
    without depending on scala.Product or other case class infrastructure.

--- crates/scala/tests/codegen.rs
+++ crates/scala/tests/codegen.rs
@@ -45,7 +45,7 @@ fn test_simple_types() {
     let scala_content = std::str::from_utf8(contents[0].1).unwrap();
 
     assert!(scala_content.contains("package com.example.test"));
-    assert!(scala_content.contains("case class Point"));
+    assert!(scala_content.contains("final class Point"));
     assert!(scala_content.contains("def add"));
     assert!(scala_content.contains("@scala.scalajs.wit.annotation.WitImport"));
 }
@@ -204,7 +204,7 @@ fn test_flags() {
     let contents: Vec<_> = files.iter().collect();
     let scala_content = std::str::from_utf8(contents[0].1).unwrap();
 
-    assert!(scala_content.contains("case class FilePerms"));
+    assert!(scala_content.contains("final class FilePerms"));
     assert!(scala_content.contains("@scala.scalajs.wit.annotation.WitFlags"));
     assert!(scala_content.contains("val read ="));
     assert!(scala_content.contains("val write ="));
